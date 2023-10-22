be05a64d30c3e52928e4501b3f1b6edcfd8a0840
mariaabg <mariaabgois@gmail.com>
2023-10-22 12:26:57 -0300

    Update Erros

--- ECOnnect/Budget/urls.py
+++ ECOnnect/Budget/urls.py
@@ -3,12 +3,12 @@ from Budget.views import *
 
 urlpatterns = [
     path('orcamento/', orcamento, name='orcamento'),  # Orçamento
-    path('', home,name=''),  # Área Disponível
+    path('', home, name=''),  # Área Disponível
     path('calculadora/', calculadora,name='calculadora'),  # Calculadora Solar
     path('suporte/', suporte,name='suporte'),  # Contato
     path('informacoes/', informacoes,name='informacoes'),  # Informações
-    path('sobrenos/', sobreNos,name='sobrenos'),  # Sobre Nós
-    path('empresas/', empresas,name='empresas'),  # Empresas Próximas A Mim
-    path('potencial/', potencial,name='potencial'),  # Potencial de Geração de Energia
-    path('area/',area,name='area')
+    path('sobrenos/', sobreNos, name='sobrenos'),  # Sobre Nós
+    path('empresas/', empresas, name='empresas'),  # Empresas Próximas A Mim
+    path('potencial/', potencial, name='potencial'),  # Potencial de Geração de Energia
+    path('area/', area, name='area')
 ]
